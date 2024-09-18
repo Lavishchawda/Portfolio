682844664e5fc57ca35d52973ebe21e0da2297cb
Lavish Chawda <lavishchawda@Lavishs-Air.lan>
2024-09-18 19:51:14 +0530

    create header design

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className="h-screen w-screen bg-red-300 text-3xl font-bold text-blue-500">Header</div>
+    <div className="bg-light-semi-color py-4 px-6 flex flex-row justify-between items-center">
+      <div className="text-white cursor-pointer">
+      Lavish
+      </div>
+      <div className='text-white cursor-pointer'>
+        <ul className="flex gap-3">
+          <li className='hover:text-yellow-300'>About</li>
+          <li className='hover:text-yellow-300'>Projects</li>
+          <li className='hover:text-yellow-300'>Skills</li>
+          <li className='hover:text-yellow-300'>Contact Me</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
